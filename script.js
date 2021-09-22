b7cd6c54d09c81300ddff62b65f952d79dd5a820
alemoleta <alemoleta@yahoo.com.br>
2021-09-22 02:20:46 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -9,20 +9,35 @@ snake[0] = {
 
 let direction = "right";
 
+//array da comidinha que cria a comidinha em locais randômicos da tela
+let food = {    
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+//criar background, desenha tela
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+//cria a cobrinha
 function criarCobrinha(){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillStyle = "green"; //colore a cobrinha
+        context.fillRect(snake[i].x, snake[i].y, box, box); //cria um retângulo
     }
 }
 
-document.addEventListener('keydown', update);
+//função de criar comidinha
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
 
+//evento que capta o comando direcional do teclado
+document.addEventListener('keydown', update);
+//função que faz com que a cobrinha não fique com 2 cabeças em caso de comando que inverte a direção do movimento
 function update (event){
     if (event.keyCode == 37 && direction != "right") direction = "left";   
     if (event.keyCode == 38 && direction != "down") direction = "up";   
@@ -31,17 +46,21 @@ function update (event){
 }
 
 function iniciarJogo(){
+    //faz com a cobrinha não suma da tela, fazendo-a retornar do outro lado quando passa do limite
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-
+    
     criarBG();
     criarCobrinha();
-
+    drawFood();
+ 
+    //coordenadas
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-
+ 
+    //faz o movimento alterando as coordenadas
     if (direction == "right") snakeX += box;
     if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY -= box;
@@ -49,6 +68,7 @@ function iniciarJogo(){
 
     snake.pop();
 
+    //cabeça da cobrinha
     let newHead = {
         x: snakeX,
         y: snakeY,
@@ -57,5 +77,5 @@ function iniciarJogo(){
     snake.unshift(newHead);
 
 }
-
+//atualiza a tela
 let jogo = setInterval(iniciarJogo, 100);
