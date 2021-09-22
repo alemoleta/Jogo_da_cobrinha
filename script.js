1c0fc029afa1f091c9232511dfc81ab8b752f7b3
alemoleta <alemoleta@yahoo.com.br>
2021-09-22 03:26:05 -0300

    definir fim do jogo

--- script.js
+++ script.js
@@ -45,13 +45,21 @@ function update (event){
     if (event.keyCode == 40 && direction != "up") direction = "down";   
 }
 
-function iniciarJogo(){
+function iniciarJogo(){  
     //faz com a cobrinha não suma da tela, fazendo-a retornar do outro lado quando passa do limite
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
+    //checar se as coordenadas se chocam para testar se a cobrinha esbarrou em si mesma
+    for (i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert ('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
